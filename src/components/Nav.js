e397a7e0e82c181bebedbbaae25d9030dce4a6dc
Flannery Wilson <fwils001@ucr.edu>
2022-11-02 17:28:53 -0700

    changed size

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ const Nav = (props) => {
   return (
     <div className="nav">
       <nav>
-        <h2>Welcome</h2>
+        <h2>Welcome!</h2>
       </nav>
     </div>
   )
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,7 +1,7 @@
 const Song = (props) => {
   return (
     <div className="song">
-      <h1>Title: {props.song.title}</h1>
+      <h2>Title: {props.song.title}</h2>
       <h2>Artist: {props.song.artist}</h2>
       <h3>Date it reached number 1: {props.song.date}</h3>
       <h3>Weeks at number 1: {props.song.weeks}</h3>
